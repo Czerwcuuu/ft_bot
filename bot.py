d9c6ba636e369155881e95a925477d6c8e03d1db
czerwiecr1999@gmail.com <aiqu4voh123>
2020-01-20 12:54:34 +0100

    1.1.0 - Mozliwosc rozwoju kazdej umiejetnosci, usprawnione dzialanie i przyspieszone.

--- bot.py
+++ bot.py
@@ -6,9 +6,70 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
+'''
+W clickerze, są 3 linijki zaczynajace sie :text,wheretomove,ele - podmien je kopiujac i wklejajac te ponizej, jezeli chcesz rozwijac dana
+umiejetnosc.
 
+ATAK
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]")
+ele = driver.find_element_by_xpath("//div[@class='training']//div[2]//div[1]//div[2]//button[1]")
 
+OBRONA
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]")
+ele = driver.find_element_by_xpath("//div[3]//div[1]//div[2]//button[1]")
 
+ROZGRYWANIE
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[4]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[4]")
+ele = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[4]/div[1]/div[2]/button[1]")
+
+KONDYCJA
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[5]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[5]")
+ele = driver.find_element_by_xpath("//div[5]//div[1]//div[2]//button[1]")
+
+CZYTANIE GRY
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[6]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[6]")
+ele = driver.find_element_by_xpath("//div[6]//div[1]//div[2]//button[1]")
+
+PRESSING
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[7]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[7]")
+ele = driver.find_element_by_xpath("//div[7]//div[1]//div[2]//button[1]")
+
+STALE FRAGMENTY
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[8]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[8]")
+ele = driver.find_element_by_xpath("//div[8]//div[1]//div[2]//button[1]")
+
+SKUTECZNOSC
+text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[9]/div[1]/div[2]/div[3]/div[2]/span[1]")
+wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[9]")
+ele = driver.find_element_by_xpath("//div[9]//div[1]//div[2]//button[1]")
+
+'''
+def clicker():
+    count = 0
+    while(1):
+        text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[8]/div[1]/div[2]/div[3]/div[2]/span[1]")
+        how_much_wait = int(re.search(r'\d+', text.text).group())
+        print(how_much_wait)
+        print(count)
+        wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[8]")
+        hover = ActionChains(driver).move_to_element(wheretomove)
+        hover.perform()
+        ele = driver.find_element_by_xpath("//div[8]//div[1]//div[2]//button[1]")
+        ele.click()
+        time.sleep(how_much_wait+2)
+        count = count + 1
+        #GDY WYWALA PRZEZ KOMuNIKATY UKONCZONEGO TRENINGU
+        '''if count == 6:
+            count = 0
+            time.sleep(10)'''
+    return
 driver = webdriver.Chrome()
 driver.get("https://footballteam.pl/")
 ele = driver.find_elements_by_xpath("/html/body/main/section[1]/div/div[5]/div/button[1]")[0]
@@ -16,24 +77,14 @@ ele.click()
 ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div[1]/h5")[0]
 ele.click()
 ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[1]/input")[0]
-ele.send_keys("login")
+ele.send_keys("fortepiany8@gmail.com")
 ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[2]/input")[0]
-ele.send_keys("password")
+ele.send_keys("aiqu4voh123")
 ele = driver.find_elements_by_xpath("//*[@id='btn-login']")[0]
 ele.click()
 time.sleep(5)
 driver.get("https://game.footballteam.pl/training")
 time.sleep(5)
 how_much_wait = 0
-while(1):
-    time.sleep(how_much_wait+3)
-    text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[5]/div[1]/div[2]/div[3]/div[2]/span[1]")
-    how_much_wait = int(re.search(r'\d+', text.text).group())
-    wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]/div[2]")
-    hover = ActionChains(driver).move_to_element(wheretomove)
-    hover.perform()
-    ele = driver.find_elements_by_xpath("/html/body/div[1]/div/main/div/div/div[2]/div[1]/div[3]/div/div[2]/button")[0]
-    ele.click()
-
-
+clicker()
 
