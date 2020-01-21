c7eb59767e6c9fd4a501b5cf5c3e7b501205e81f
Czerwcuuu <czerwiecr1999@gmail.com>
2020-01-21 13:13:36 +0100

    1.2 - mozliwosc rozwoju dwoch umiejetnosci na raz.

--- bot.py
+++ bot.py
@@ -52,6 +52,8 @@ wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main
 ele = driver.find_element_by_xpath("//div[9]//div[1]//div[2]//button[1]")
 
 '''
+#clicker, gdy chcesz ulepszac tylko jedna umiejetnosc
+'''
 def clicker():
     count = 0
     while(1):
@@ -67,6 +69,40 @@ def clicker():
         time.sleep(how_much_wait+2)
         count = count + 1
         #GDY WYWALA PRZEZ KOMuNIKATY UKONCZONEGO TRENINGU
+        if count == 6:
+            count = 0
+            time.sleep(10)
+    return'''
+#doubleclicker gdy chcesz ulepszac dwie umiejetnosci na raz
+def doubleclicker():
+    count = 0
+    while(1):
+        text = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[4]/div[1]/div[2]/div[3]/div[2]/span[1]")
+        text2 = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[2]/div[3]/div[2]/span[1]")
+        how_much_wait = int(re.search(r'\d+', text.text).group())
+        how_much_wait2 = int(re.search(r'\d+', text2.text).group())
+        if how_much_wait2 > how_much_wait:
+            how_much_wait = how_much_wait2
+        
+        print(how_much_wait)
+        print(how_much_wait2)
+        print(count)
+        
+        wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[4]")
+        hover = ActionChains(driver).move_to_element(wheretomove)
+        hover.perform()
+        ele = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[4]/div[1]/div[2]/button[1]")
+        ele.click()
+
+        wheretomove =  driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[3]")
+        hover = ActionChains(driver).move_to_element(wheretomove)
+        hover.perform()
+        ele = driver.find_element_by_xpath("//div[3]//div[1]//div[2]//button[1]")
+        ele.click()
+        
+        time.sleep(how_much_wait+2)
+        count = count + 1
+        #GDY WYWALA PRZEZ KOMuNIKATY UKONCZONEGO TRENINGU
         '''if count == 6:
             count = 0
             time.sleep(10)'''
@@ -78,7 +114,7 @@ ele.click()
 ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div[1]/h5")[0]
 ele.click()
 ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[1]/input")[0]
-ele.send_keys("login")
+ele.send_keys("login@gmail.com")
 ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[2]/input")[0]
 ele.send_keys("password")
 ele = driver.find_elements_by_xpath("//*[@id='btn-login']")[0]
@@ -87,5 +123,5 @@ time.sleep(5)
 driver.get("https://game.footballteam.pl/training")
 time.sleep(5)
 how_much_wait = 0
-clicker()
+doubleclicker()
 
