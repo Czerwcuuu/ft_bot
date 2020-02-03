f90c6a61678429e40ccd2971075ef7922d4e6dba
Czerwcuuu <czerwiecr1999@gmail.com>
2020-02-03 15:44:38 +0100

    03.02 - 15:44 (3) 1.5

--- botv2.py
+++ botv2.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QWidget,QLabel,QGridLayout,QLineEdit,QPushButton,QHBoxLayout
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread
 from PyQt5.QtGui import QIcon
 import time
 import re
@@ -13,7 +13,90 @@ from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import InvalidElementStateException,ElementClickInterceptedException,NoSuchElementException
 
+class StartBotThread(QThread):
+
+    def __init__(self,login,password,a,b):
+        QThread.__init__(self)
+        self.login = login
+        self.password = password
+        self.a = a;
+        self.b = b;
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        driver = webdriver.Chrome()
+        driver.get("https://footballteam.pl/")
+        ele = driver.find_elements_by_xpath("/html/body/main/section[1]/div/div[5]/div/button[1]")[0]
+        ele.click()
+        ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div[1]/h5")[0]
+        ele.click()
+        ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[1]/input")[0]
+        ele.send_keys(self.login)
+        ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[2]/input")[0]
+        ele.send_keys(self.password)
+        ele = driver.find_elements_by_xpath("//*[@id='btn-login']")[0]
+        ele.click()
+        time.sleep(5)
+        driver.get("https://game.footballteam.pl/training")
+        time.sleep(5)
+        how_much_wait = 0
+        count = 0
+        try:
+            print("Working")
+            finallmoney = "";
+            money = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/header[1]/div[2]/ul[1]/li[3]/p[1]")
+            text = driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.a}]/div[1]/div[2]/div[3]/div[2]/span[1]")
+            text2 = driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.b}]/div[1]/div[2]/div[3]/div[2]/span[1]")
+            cost1 = driver.find_element_by_xpath(f"//div[{self.a}]//div[1]//div[2]//div[3]//div[1]//span[1]")
+            cost2 = driver.find_element_by_xpath(f"//div[{self.b}]//div[1]//div[2]//div[3]//div[1]//span[1]")
+            how_much_wait = int(re.search(r'\d+', text.text).group())
+            how_much_wait2 = int(re.search(r'\d+', text2.text).group())
+            price1=int(re.search(r'\d+', cost1.text).group())
+            price2=int(re.search(r'\d+', cost2.text).group())
+            money = re.findall(r'\d+', money.text)
+            for x in money:
+                finallmoney +=x
+            money = int(finallmoney)
+            print(f"Na koniec treningu 1 umiejetnosci trzeba czekac:{how_much_wait}, cena treningu:{price1}")
+            print(f"Na koniec treningu 2 umiejetnosci trzeba czekac:{how_much_wait2}, cena treningu:{price2}")
+            if price2 > price1:
+                price1 = price2
+            if how_much_wait2 > how_much_wait:
+                how_much_wait = how_much_wait2
+            if price1>money:
+                print("Koniec pieniedzy na koncie w grze, koncze dzialanie bota")
+                sys.exit(0)
+            print(f"Ilosc pieniedzy na koncie:{money}")
+            count = count + 2
+            print(f"BOT wykonał już {count} treningow!")
+            
+            wheretomove =  driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.a}]")
+            hover = ActionChains(driver).move_to_element(wheretomove)
+            hover.perform()
+            ele = driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.a}]/div[1]/div[2]/button[1]")
+            ele.click()
+
+            wheretomove =  driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.b}]")
+            hover = ActionChains(driver).move_to_element(wheretomove)
+            hover.perform()
+            ele = driver.find_element_by_xpath(f"//div[{self.b}]//div[1]//div[2]//button[1]")
+            ele.click()
+            
+            time.sleep(how_much_wait+2)
+        except InvalidElementStateException as e:
+            print("===BOT nie moze wcisnac guzika! Ponizej wiecej szczegolow!===")
+            print(e)
+        except NoSuchElementException as e:
+            print("===BOT nie moze znalezc guzika! Ponizej wiecej info!===")
+            print(e)
+        except ElementClickInterceptedException as e:
+            print("===BOT nie moze wcisnac guzika, poniewaz jest czyms zakryty!===")
+            print(e)
+
 class Bot(QWidget):
+
     def __init__(self,parent=None):
         super().__init__(parent)
         self.interfejs()
@@ -22,6 +105,7 @@ class Bot(QWidget):
         self.b = 3
         self.login = "login@interia.pl" #Tu wpisz wybrany login
         self.password = "xxx"    #Tu wpisz wybrany haslo
+        
 
     def interfejs(self):
         #etykiety
@@ -79,90 +163,18 @@ class Bot(QWidget):
         self.close()
 
     def wybor(self):
+        self.StartBotThread = StartBotThread(self.login,self.password,self.a,self.b)
         nadawca = self.sender()
         try:
             if nadawca.text() == "&START":
                 if int(self.aktualnie_trenowane.text())>1 and int(self.aktualnie_trenowane.text()) < 10 and int(self.aktualnie_trenowane2.text()) < 10 and int(self.aktualnie_trenowane2.text()) > 1:
                     self.a = int(self.aktualnie_trenowane.text())
                     self.b = int(self.aktualnie_trenowane2.text())
-                    print(self.a)
-                    print(self.b)
-                    self.StartBot()
+                    self.StartBotThread.start()
                 else:
                     print("Nie ma takiej opcji")
         except ValueError:
-            print("Zla wartosc")
-    def StartBot(self):
-        driver = webdriver.Chrome()
-        driver.get("https://footballteam.pl/")
-        ele = driver.find_elements_by_xpath("/html/body/main/section[1]/div/div[5]/div/button[1]")[0]
-        ele.click()
-        ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[1]/div[1]/h5")[0]
-        ele.click()
-        ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[1]/input")[0]
-        ele.send_keys(self.login)
-        ele = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[2]/div[1]/form/div[2]/input")[0]
-        ele.send_keys(self.password)
-        ele = driver.find_elements_by_xpath("//*[@id='btn-login']")[0]
-        ele.click()
-        time.sleep(5)
-        driver.get("https://game.footballteam.pl/training")
-        time.sleep(5)
-        how_much_wait = 0
-        count = 0
-        while(1):
-            try:
-                print("Working")
-                finallmoney = "";
-                money = driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/header[1]/div[2]/ul[1]/li[3]/p[1]")
-                text = driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.a}]/div[1]/div[2]/div[3]/div[2]/span[1]")
-                text2 = driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.b}]/div[1]/div[2]/div[3]/div[2]/span[1]")
-                cost1 = driver.find_element_by_xpath(f"//div[{self.a}]//div[1]//div[2]//div[3]//div[1]//span[1]")
-                cost2 = driver.find_element_by_xpath(f"//div[{self.b}]//div[1]//div[2]//div[3]//div[1]//span[1]")
-                how_much_wait = int(re.search(r'\d+', text.text).group())
-                how_much_wait2 = int(re.search(r'\d+', text2.text).group())
-                price1=int(re.search(r'\d+', cost1.text).group())
-                price2=int(re.search(r'\d+', cost2.text).group())
-                money = re.findall(r'\d+', money.text)
-                for x in money:
-                    finallmoney +=x
-                money = int(finallmoney)
-                print(f"Na koniec treningu 1 umiejetnosci trzeba czekac:{how_much_wait}, cena treningu:{price1}")
-                print(f"Na koniec treningu 2 umiejetnosci trzeba czekac:{how_much_wait2}, cena treningu:{price2}")
-                if price2 > price1:
-                    price1 = price2
-                if how_much_wait2 > how_much_wait:
-                    how_much_wait = how_much_wait2
-                if price1>money:
-                    print("Koniec pieniedzy na koncie w grze, koncze dzialanie bota")
-                    sys.exit(0)
-                print(f"Ilosc pieniedzy na koncie:{money}")
-                count = count + 2
-                print(f"BOT wykonał już {count} treningow!")
-                
-                wheretomove =  driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.a}]")
-                hover = ActionChains(driver).move_to_element(wheretomove)
-                hover.perform()
-                ele = driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.a}]/div[1]/div[2]/button[1]")
-                ele.click()
-
-                wheretomove =  driver.find_element_by_xpath(f"/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/div[{self.b}]")
-                hover = ActionChains(driver).move_to_element(wheretomove)
-                hover.perform()
-                ele = driver.find_element_by_xpath(f"//div[{self.b}]//div[1]//div[2]//button[1]")
-                ele.click()
-                
-                time.sleep(how_much_wait+2)
-            except InvalidElementStateException as e:
-                print("===BOT nie moze wcisnac guzika! Ponizej wiecej szczegolow!===")
-                print(e)
-            except NoSuchElementException as e:
-                print("===BOT nie moze znalezc guzika! Ponizej wiecej info!===")
-                print(e)
-            except ElementClickInterceptedException as e:
-                print("===BOT nie moze wcisnac guzika, poniewaz jest czyms zakryty!===")
-                print(e)
-        return
+            print("Zla wartosc")        
         
         
         
@@ -173,6 +185,7 @@ if __name__ == '__main__':
     okno = Bot()
     sys.exit(app.exec_())
         
+
         
         
         
